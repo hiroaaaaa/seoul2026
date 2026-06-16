9cb4adfe28e9e55c22ef9406c8d68812c4842696
hiroaaaaa <hiroqqq@gmail.com>
2026-06-16 22:44:02 +0800

    Update service worker cache for template page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='seoul-family-2026-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE_NAME='seoul-family-2026-v2';
+const ASSETS=['./','./index.html','./update.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return res;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});
