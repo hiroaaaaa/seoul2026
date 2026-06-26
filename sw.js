3fe9f064078f756ad18088ba5cdb9d5e66a32223
hiroaaaaa <hiroqqq@gmail.com>
2026-06-26 13:43:18 +0800

    Bump Seoul itinerary cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='seoul-family-2026-v3';
+const CACHE_NAME='seoul-family-2026-v4';
 const ASSETS=['./','./index.html','./data.js','./update.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
